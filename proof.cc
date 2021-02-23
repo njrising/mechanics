2a1c243a2d70f57c6e38c15031e9650b8802cd95
njrising <njrising@outlook.com>
2021-02-22 22:07:32 -0500

    ...

--- proof.cc
+++ proof.cc
@@ -1,9 +1,17 @@
-#include <iostream>
-#include <array>
-#include <algorithm>
-
-//#include <chrono>
-#include <cstdio>
+#include <iostream>  // output
+#include <array>     // array
+#include <algorithm> // stable_sort
+// Proof of a formula found in advanced mechanics textbooks
+// The following equation is actually a set of 81 eqautions
+// e_ijm * e_klm = d_ik * d_jl - d_il * d_jk
+// Preferring a computational approach to verifying the relationship,
+// We can show the relationship is true through brute computation.
+//
+// Author: Nathan Rising
+// 
+// I considered different methods to writing the epsilon function
+// first through sorting and lambdas, however a simpler approach 
+// uses only if statements and is considerably more efficient
 
 // kronecker delta 
 int k_delta(int i, int j){
@@ -12,9 +20,10 @@ int k_delta(int i, int j){
 }
 
 // permutation tensor
-int epsilon(std::array<int,3> &v,int n=0){
-	if(v[0] == v[1] || v[0] == v[2] || v[1] == v[2]){return 0;}
+int epsilon(int i,int j,int k,int n=0){
+	if(i == j || i == k || j == k){return 0;}
 	else{
+		std::array<int,3> v = {i,j,k};
 		std::stable_sort(v.begin(),v.end(),[&n](int a,int b){n+=(a<b);return a<b;});
 		return (n % 2 == 0) ? 1 : -1;
 	}
@@ -30,34 +39,19 @@ int epsilon_(int i,int j,int k){
 	else if(i==2 && j==1 && k==3){return -1;}
 }
 
-
 int main(){
-
-	//std::array<int,3> arr = {3,2,1};
-	
-//	auto start = std::chrono::high_resolution_clock::now();
-	
-//	for(int i = 0;i<1000000;++i){
-//		epsilon_(3,2,1);
-//	}
-//	auto finish = std::chrono::high_resolution_clock::now();
-//	std::chrono::duration<double> elapsed = finish - start;
-//	std::cout<<"Elapsed time: "<<elapsed.count()<<"s\n";
-	
-	int epsilon_result[81] = {};
-	int delta_result[81] = {};
-	int index[81][4] = {};
-	
-	int count = -1;
+	int epsilon_result[81] = {};  // epsilon values
+	int delta_result[81] = {};    // delta values
+	int count = -1;				  	
 	uint8_t i,j,k,l,m;
 	i=j=k=l=m=1;
+	// looping through...
 	for(i = 1;i < 4;++i){
 		for(j = 1;j < 4;++j){
 			for(k = 1;k < 4;++k){
 				for(l = 1;l < 4;++l){
 					count++;
 					for(m=1;m < 4;++m){
-						//index[count][0] = i;index[count][1] = j;index[count][2] = k;index[count][3] = l;
 						epsilon_result[count] += epsilon_(i,j,m) * epsilon_(k,l,m); 
 					}
 					delta_result[count] = k_delta(i,k) * k_delta(j,l) - k_delta(i,l) * k_delta(j,k);
@@ -65,13 +59,9 @@ int main(){
 			}
 		}
 	}
-	
+	// print results.
 	for(int i=0;i<81;++i){
 		std::cout<<epsilon_result[i]<<' '<<delta_result[i]<<'\n';
 	}
-	
-	
-	
-
 	return 0;
 }

8392be6b804c1ee9366424e5d6833700f49b17fd
njrising <njrising@outlook.com>
2021-03-06 16:15:51 -0500

    ...

--- proof.cc
+++ proof.cc
@@ -14,7 +14,7 @@
 // uses only if statements and is considerably more efficient
 
 // kronecker delta 
-int k_delta(int i, int j){
+std::uint8_t k_delta(int i, int j){
 	if(i == j){return 1;}
 	else{return 0;}	
 }
@@ -29,7 +29,7 @@ int epsilon(int i,int j,int k,int n=0){
 	}
 }
 // faster permutation tensor
-int epsilon_(int i,int j,int k){
+std::int8_t epsilon_(int i,int j,int k){
 	if(i == j || i == k || j == k){return 0;}
 	else if(i==1 && j==2 && k==3){return 1;}
 	else if(i==2 && j==3 && k==1){return 1;}
@@ -40,10 +40,10 @@ int epsilon_(int i,int j,int k){
 }
 
 int main(){
-	int epsilon_result[81] = {};  // epsilon values
-	int delta_result[81] = {};    // delta values
-	int count = -1;				  	
-	uint8_t i,j,k,l,m;
+	std::int16_t epsilon_result[81] = {};  // epsilon values
+	std::int16_t delta_result[81] = {};    // delta values
+	std::int8_t count = -1;				  	
+	std::uint8_t i,j,k,l,m;
 	i=j=k=l=m=1;
 	// looping through...
 	for(i = 1;i < 4;++i){
@@ -51,7 +51,7 @@ int main(){
 			for(k = 1;k < 4;++k){
 				for(l = 1;l < 4;++l){
 					count++;
-					for(m=1;m < 4;++m){
+					for(m = 1;m < 4;++m){
 						epsilon_result[count] += epsilon_(i,j,m) * epsilon_(k,l,m); 
 					}
 					delta_result[count] = k_delta(i,k) * k_delta(j,l) - k_delta(i,l) * k_delta(j,k);
@@ -60,7 +60,7 @@ int main(){
 		}
 	}
 	// print results.
-	for(int i=0;i<81;++i){
+	for(i=0;i<81;++i){
 		std::cout<<epsilon_result[i]<<' '<<delta_result[i]<<'\n';
 	}
 	return 0;
